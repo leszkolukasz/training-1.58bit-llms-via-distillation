3107362534417f832a50dd42f6eeda07199671f0
Filip Mateńko <filip@matenko.pl>
2025-06-20 17:31:38 +0200

    Checkpoint

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 QWEN_MODEL_ID = "Qwen/Qwen3-0.6B"
 SMOL_MODEL_ID = "HuggingFaceTB/SmolLM2-135M-Instruct"
-AMBER_DATASET_PATH = "./data/amber"
+# AMBER_DATASET_PATH = "./data/amber"
+AMBER_DATASET_PATH = "./data/amber_small_10"
 EPSILON = 1e-6
-BATCH_SIZE = 1
+BATCH_SIZE = 2
