a047616aa63060fbb5771be7da83c1f5eab7d663
Filip Mateńko <filip@matenko.pl>
2025-06-25 22:09:34 +0200

    Correct LIM

---
+++ src/layer_analysis/what_to_quantize.py
@@ -0,0 +1,117 @@
+from src.models import *
+from src.datamodules import *
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import numpy as np 
+from datasets import load_dataset
+from tqdm import tqdm
+
+# Script for analyzing which layers to quantize in a model
+
+def LIM(model):
+    
+    dataset = load_dataset(
+            "json", data_dir=AMBER_DATASET_PATH, streaming=True
+        ).with_format("torch")
+    dataloader = DataLoader(
+            dataset['train'],
+            batch_size=BATCH_SIZE,
+            )
+    input_activations = {}
+    output_activations = {}
+    
+    handles = []
+    
+    def make_hooks(layer_idx):
+        def input_hook(module, inp):
+            input_activations[layer_idx] = inp[0].detach().cpu().numpy()
+        def output_hook(module, inp, out):
+            output_activations[layer_idx] = out.detach().cpu().numpy()
+        return input_hook, output_hook
+    
+    for index, layer in enumerate(model.modules()):
+        if isinstance(layer, nn.Linear):
+            input_hook, output_hook = make_hooks(index)
+            handles.append(layer.register_forward_pre_hook(input_hook))
+            handles.append(layer.register_forward_hook(output_hook))
+    
+    layer_sims = {f"{name}": [] for name, module in model.named_modules() if isinstance(module, nn.Linear)}
+    
+    device = "cpu"
+    model.to(device)
+    
+    for batch in tqdm(dataloader):
+        inputs = {
+            "input_ids": batch["token_ids"].to(device),
+            "attention_mask": batch["attention_mask"].to(device) if "attention_mask" in batch else None
+        }
+        
+        with torch.no_grad():
+            model(**inputs)
+            
+        for layer_idx, (name, module) in enumerate(model.named_modules()):
+            if isinstance(module, nn.Linear):
+                inp = input_activations[layer_idx]
+                out = output_activations[layer_idx]
+                
+                batch_sims = [
+                ]
+                print(inp.shape, out.shape)
+                
+                for k in range(inp.shape[0]):
+                    for b in range(inp.shape[1]):
+                        sim = torch.cosine_similarity(
+                        torch.tensor(inp[k, b, :]),
+                            torch.tensor(out[k, b, :]),
+                            dim=0
+                            ).item()
+                        batch_sims.append(sim)
+                layer_sims[f"{name}"].append(sum(batch_sims) / len(batch_sims))
+                
+    for handle in handles:
+        handle.remove()
+            
+    lim_scores = {layer: -np.mean(sims) for layer, sims in layer_sims.items()}
+        
+    return lim_scores
+
+def ZD(model): 
+    
+    scores = dict()
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Linear):
+            weights = module.weight
+            std = weights.std().item()
+            mean = weights.mean().item()
+            z_score = (weights - mean) / (std + EPSILON)
+            z_score = z_score.flatten()
+            greater_than_one = (z_score > 1).sum().item()
+            scores[name] = greater_than_one / len(z_score)
+    return scores
+
+def analyze_layers_to_quantize(model_name: str, score_function):
+    
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model.eval()
+    
+    scores = score_function(model)  
+    
+    return sorted(scores.items(), key=lambda item: item[1], reverse=True)
+
+if __name__ == "__main__":
+    model_name = SMOL_MODEL_ID
+    
+    scores = analyze_layers_to_quantize(model_name, LIM)
+    
+    for layer, score in scores:
+        print(f"Layer: {layer}, Score: {score:.4f}")
+    
+    # model = AutoModelForCausalLM.from_pretrained(model_name)
+    # for layer in model.model.layers:
+    #     print(layer)
+    
+    
+    
+    
+    
+    
