382eeac4ea77f3d71cb435fd05399e9657818c67
Łukasz Leszko <leszko.lucas@gmail.com>
2025-06-26 20:56:00 +0200

    Update constants

--- src/constants.py
+++ src/constants.py
@@ -10,7 +10,7 @@ BATCH_SIZE = config("BATCH_SIZE", default=4, cast=int)
 MAX_SEQUENCE_LENGTH = config("MAX_SEQUENCE_LENGTH", default=1024, cast=int)
 SAVE_EVERY_N_STEPS = config("SAVE_EVERY_N_STEPS", default=5000, cast=int) # step is finished when optimizer is called
 ACCUMULATE_GRADIENT_FOR_N_SAMPLES = 16
-RUN_NAME_SUFFIX = "_LIM_100_most"
+RUN_NAME_SUFFIX = ""
 EPSILON = 1e-6
 INITIAL_LR = 3e-4
-PERCENTAGE_OF_LAYERS_TO_QUANTIZE = 1.
+PERCENTAGE_OF_LAYERS_TO_QUANTIZE = 0.25
